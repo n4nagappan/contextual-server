15b7ba01e9fb658cf97e5bb505fe62fab90e4215
Nagappan <n4nagappan@gmail.com>
2015-11-29 15:12:46 +0000

    Lots of changes

--- server.js
+++ server.js
@@ -3,7 +3,13 @@
 var request = require('request');
 var extractor = require('unfluff');
 var restify = require('restify');
-var url = 'http://www.newyorker.com/magazine/2015/02/23/shape-things-come';
+var extractKeywords = require('./lib/extractKeywords.js');
+var DEFAULT_URL = 'http://www.newyorker.com/magazine/2015/02/23/shape-things-come';
+var DEFAULT_TERM = 'Jonathan Ive';
+
+var wikiParser = require('wiki-infobox-parser');
+
+//var DEFAULT_URL = 'http://waitbutwhy.com/2014/09/muhammad-isis-iraqs-full-story.html';
  
 var server = restify.createServer({
   name: 'contextual'
@@ -13,16 +19,46 @@ server.use(restify.queryParser());
 //server.use(restify.bodyParser());
  
 server.get('/contents', function (req, res, next) {
-  res.send(req.params);
+  var url = req.params.url;
+  //console.log( url );
 
-  request(url, function(err, res, body){
+  request(url || DEFAULT_URL , function(err, urlRes, body){
       if(err){
           return next(err);
       }
-      data = extractor(my_html_data);
-      console.log(data);
-      return next(data);
+
+      var data = extractor(body);
+      extractKeywords.getKeywords(data.text, function(err, keywords){
+        data.extractedKeywords = keywords;
+        console.log( keywords.interest);
+        return res.send(data);
+      });
   });
+});
+
+
+server.get('/search', function(req, res, next){
+  var term = req.params.q || DEFAULT_TERM ;
+  //console.log( term);
+
+  wikiParser('Jonathan Ive', function(err, result) {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log(result);
+      res.send( result );
+    }
+  });
+  //var wikiUrl = 'https://en.wikipedia.org/wiki/Special:Search?search=' + encodeURIComponent(term);
+  //request( wikiUrl , function(err, urlRes, body){
+  //    if(err){
+  //        return next(err);
+  //    }
+
+  //    var data = extractor(body);
+ 
+  //    res.send(data.text);
+  //});
 });
  
 server.listen(8080, function () {
